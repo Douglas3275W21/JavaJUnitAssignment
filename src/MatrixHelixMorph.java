950fd3c5ff96e609f3ce175be7ac052e533bdc3d
Rez G <mrghaeli@yahoo.com>
2021-03-12 08:24:52 -0800

    first comment

---
+++ src/MatrixHelixMorph.java
@@ -0,0 +1,10 @@
+public class MatrixHelixMorph {
+
+    /**
+     * @param inMatrix
+     * @return a matrix that is morphed into a helix version of inMatrix
+     */
+    public static int[][] helix( int[][] inMatrix) {
+        return null; //MODIFY THIS METHOD
+    }
+}
